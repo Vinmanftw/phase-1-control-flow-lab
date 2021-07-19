58e2cd9a4d1f6a1c579834d619a6d0ff4d537f04
Vincent <vao2002@gmail.com>
2021-07-19 16:20:50 -0500

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someNumber){
+  if (someNumber <= 400) {
+    return 'This one is on me!';
+  }
+  else if (someNumber>2500){
+    return 'No can do.';
+  }
+  else if (someNumber>2000){
+    return 'I will gladly take your thirty bucks.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(destination){
+  if(destination === 'NYC'){
+    return 'Ok, sounds good.';
+  }
+  return "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let response;
+  switch(tip) {
+    case 'generous':
+       response = 'Thank you so much.';
+      break;
+    case 'not as generous':
+       response = 'Thank you.'
+      break;
+    default:
+      response = 'Bye.';
+  }
+  return response;
 }
